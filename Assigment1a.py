41b4a051a0f3817586c052bf10adc21bf8d15f92
Mitanshu1-eng <mitanshuagrawal345@gmail.com>
2025-09-21 17:12:37 +0530

    Add files via upload

---
+++ Assigment1a.py
@@ -0,0 +1,6 @@
+a=int(input("Enter your first number: "))
+b=int(input("Enter your second number: "))
+print("Addition:",a+b)
+print("Subraction:",a-b)
+print("Multiplication:",a*b)
+print("Divsion:",a/b)
