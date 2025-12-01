9bb1b54e75e4bf83706e8c2e2e7b1e76cda10ecd
Mitanshu1-eng <mitanshuagrawal345@gmail.com>
2025-12-01 13:06:12 +0530

    Delete import math.py

--- import math.py
+++
@@ -1,9 +0,0 @@
-import math
-
-num = float(input("Enter a number: "))
-sqrt_value = math.sqrt(num)
-log_value = math.log(num)
-sine_value = math.sin(num)
-print("Square root:", sqrt_value)
-print("Logarithm:", log_value)
-print("Sine:", sine_value)
